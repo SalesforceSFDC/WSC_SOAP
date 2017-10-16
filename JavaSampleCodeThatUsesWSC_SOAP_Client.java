35654565cdef5226c9c588b7930b2c97b912c14b
Vuk Dukic <vukdukic@Vuks-MacBook-Air.local>
2017-10-16 09:32:26 -0700

    first commit

---
+++ JavaSampleCodeThatUsesWSC_SOAP_Client.java
@@ -0,0 +1,7 @@
+package com.example.samples;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import com.sforce.soap.enterprise.DeleteResult;
